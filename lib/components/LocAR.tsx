128c60dcf7689d85267c170d112c432115ea5fa4
Nick Whitelegg <nickw4426@gmail.com>
2026-08-08 17:41:29 +0100

    Add device orientation

--- lib/components/LocAR.tsx
+++ lib/components/LocAR.tsx
@@ -2,8 +2,8 @@
 import { useEffect } from 'react';
 import { App } from 'locar';
 import * as React from 'react';
-import { useState } from 'react';
-import { useThree } from '@react-three/fiber';
+import { useState, useRef } from 'react';
+import { useThree, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
 const LocarContext = React.createContext({});
@@ -13,20 +13,25 @@ import type { LocARInfo, LocARProps } from '../types';
 export default function LocAR({ options, fakeLon, fakeLat, elevation, onGpsUpdate, children }: LocARProps) {
 
     const [locar, setLocar] = useState<LocARInfo | null>(null);
+    const app = useRef<App | null>(null);
 
     const { camera, gl, scene } = useThree();
+
+    useFrame(() => {
+        app.current?.deviceOrientationControls?.update();
+    });
 
     useEffect(() => {
         setupLocar();
 
         async function setupLocar() {
 
-            const app = new App({ ...(options || {}), threeObjects: { camera: camera as THREE.PerspectiveCamera, scene, renderer: gl } });
-            const tmpLocarObject = await app.start();
+            app.current = new App({ ...(options || {}), threeObjects: { camera: camera as THREE.PerspectiveCamera, scene, renderer: gl } });
+            const tmpLocarObject = await app.current.start();
 
             tmpLocarObject.on("gpsupdate", (position: GeolocationPosition) => {
                 // Do not provide locar to children until we have an initial position
-                setLocar({ locar: tmpLocarObject, cameraFeedDimensions: app.cameraFeedDimensions });
+                setLocar({ locar: tmpLocarObject, cameraFeedDimensions: app.current!.cameraFeedDimensions });
                 onGpsUpdate?.(position);
             });
 
